3783f8f5793e13cf43012aa6fdb86e2ef5d8ae73
Javier Zaleta Martínez <94091554+jzaleta@users.noreply.github.com>
2023-07-09 00:06:37 -0600

    Update init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,24 @@
+--  Toggle opening and closing kitty with a hotkey
+-- from: https://github.com/kovidgoyal/kitty/issues/45#issuecomment-568920629
+
 hs.hotkey.bind({"ctrl"}, "space", function()
+    local app = hs.application.get("kitty")
+    if app then
+        if not app:mainWindow() then
+            app:selectMenuItem({"kitty", "New OS window"})
+        elseif app:isFrontmost() then
+            app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus("kitty")
+    end
+end)
+
+-- Toggle opening and closing finder with a hotkey as well
+
+hs.hotkey.bind({"ctrl"}, "f", function()
     local app = hs.application.get("Finder")
     if app then
         if not app:mainWindow() then
